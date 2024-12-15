5a2b8cb7537fdaef836dd23df74ef3aa9641116c
Taha-Hasemi <kiziltahaaslan@gmail.com>
2024-12-15 13:54:02 +0330

    add: commode 1 and 2, temperation, brightness and plan a

--- MainProcessor/MainProcessor.ino
+++ MainProcessor/MainProcessor.ino
@@ -6,12 +6,15 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-const char* ssid = "...";
-const char* password = "...";
-const char* mqtt_server = "...";
+const char* ssid = "ssid";
+const char* password = "pass";
+const char* mqtt_server = "0.0.0.0";
+const int mqtt_port = 1883;
 
+unsigned long sleepMillis;
 unsigned long startMillis;
 unsigned long startMillis2;
+unsigned long startMillis3;
 unsigned long currentMillis;
 const unsigned long period = 10000;
 
@@ -28,7 +31,6 @@ int value = 0;
 #define ECHO_PIN D2
 #define led D3
 #define MAX_DISTANCE 400
-// const uint64_t pipe = 0xe7e7e7e7e7LL;
 const uint64_t pipe = 0xE84D84FFLL;
 const uint64_t pipe2 = 0xE84D85FFLL;
 
@@ -38,12 +40,19 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 float duration, distance;
 
 struct data {
-  byte thermometere;
-  byte brightness;
+  float thermometere;
+  uint8_t brightness;
   bool lamp;
+  bool commode1;
+  bool commode2;
 } a;
 
-bool lampStatus = true;
+float thermometere;
+uint8_t brightness;
+bool lampStatus = false;
+bool commode1 = true;
+bool commode2 = true;
+bool planA = false;
 
 void setup_wifi() {
   delay(10);
@@ -92,27 +101,26 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  // if (topic == "lamp")
-  lampStatus = (char)payload[0] == '1';
+  if ((String)topic == "lamp") {
+    lampStatus = (char)payload[0] == '1';
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(D3, OUTPUT);
-  // pinMode(3, OUTPUT);
-  // pinMode(5, OUTPUT);
 
   radio.begin();
   radio.setChannel(84);            // Channel(0 ... 127)
-  radio.setDataRate(RF24_1MBPS);   // RF24_250KBPS, RF24_1MBPS, RF24_2MBPS)
+  radio.setDataRate(RF24_2MBPS);   // RF24_250KBPS, RF24_1MBPS, RF24_2MBPS)
   radio.setPALevel(RF24_PA_HIGH);  //RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm
   radio.openReadingPipe(0, pipe);
   radio.openWritingPipe(pipe2);
   radio.startListening();
 
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
+  client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 
   digitalWrite(led, 1);
@@ -121,16 +129,60 @@ void setup() {
     digitalWrite(led, 0);
   }
 
+  while (!radio.available()) {}
 
-  // if(!radio.available())
-  //   digitalWrite(2,1);
+  radio.read(&a, sizeof(a));
+  thermometere = a.thermometere;
+  brightness = a.brightness;
+  commode1 = a.commode1;
+  commode2 = a.commode2;
+  lampStatus = a.lamp;
 }
 
 void loop() {
+  currentMillis = millis();
+
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
+
+  if (currentMillis - startMillis >= 500) {
+    if (radio.available()) {
+      radio.read(&a, sizeof(a));
+      thermometere = a.thermometere;
+      brightness = a.brightness;
+      commode1 = a.commode1;
+      commode2 = a.commode2;
+    }
+    startMillis = currentMillis;
+  }
+
+  if (currentMillis - startMillis3 >= 800) {
+    client.publish("commode1", commode1 ? "1" : "0");
+    client.publish("commode2", commode2 ? "1" : "0");
+    char* buffer = (char*)malloc(20 * sizeof(char));
+    ltoa(sonar.ping_cm(), buffer, 5);
+    client.publish("distance", buffer);
+    dtostrf(thermometere, 6, 2, buffer);
+    client.publish("temperature", buffer);
+    itoa(brightness, buffer, 5);
+    client.publish("brightness", buffer);
+
+    if (planA) {
+      client.publish("planA", "alarm");
+      planA = false;
+    }
+
+    startMillis3 = currentMillis;
+  }
+
+  if (sonar.ping_cm() <= 50) {
+    planA = true;
+    digitalWrite(led, 1);
+  } else {
+    digitalWrite(led, 0);
+  }
 
   if (currentMillis - startMillis2 >= 100) {
     radio.stopListening();
@@ -139,24 +191,6 @@ void loop() {
     radio.startListening();
     startMillis2 = currentMillis;
   }
-
-  currentMillis = millis();
-
-  if (sonar.ping_cm() <= 50) {
-    //Serial.println(sonar.ping_cm());
-    // digitalWrite(2, 1);
-    // Serial.println("alarm");
-  } else {
-    // digitalWrite(2, 0);
-  }
-
-  if (currentMillis - startMillis >= period) {
-    if (radio.available()) {
-      radio.read(&a, sizeof(a));
-      Serial.println((String)a.thermometere + " || " + a.brightness + " || " + sonar.ping_cm());
-    }
-    startMillis = currentMillis;
-  }
 }
 
 void reconnect() {
@@ -164,10 +198,12 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
+    String clientId = "MainESP8266";
+    // clientId += String(random(0xffff), HEX);
+    String userName = "username";
+    String pass = "pass";
     // Attempt to connect
-    if (client.connect(clientId.c_str())) {
+    if (client.connect(clientId.c_str(), userName.c_str(), pass.c_str())) {
       digitalWrite(led, 1);
       delay(500);
       digitalWrite(led, 0);
@@ -179,7 +215,6 @@ void reconnect() {
       digitalWrite(led, 1);
       delay(500);
       digitalWrite(led, 0);
-      delay(500);
 
       Serial.println("connected");
       // Once connected, publish an announcement...
